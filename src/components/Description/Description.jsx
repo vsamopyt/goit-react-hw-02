f21f6f70455efc15abb116e6bec67d85bd249eba
v.samopyt <v.samopyt14@gmail.com>
2024-06-22 08:36:29 +0300

    comment-04
    add css to component Description

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,8 +1,10 @@
-export default function Description ({title, text}) {
-    return (
-        <div>
-            <h2>{title}</h2>
-            <p>{text}</p>
-        </div>
-    )
+import clsx from 'clsx';
+import css from './Description.module.css';
+export default function Description({ title, text }) {
+  return (
+    <div className={clsx(css['descriptionContainer'])}>
+      <h2>{title}</h2>
+      <p>{text}</p>
+    </div>
+  );
 }
